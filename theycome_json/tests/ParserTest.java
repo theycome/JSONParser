d0561fdb34c99a65998c720ac36a12f16796851d
Theycome <theycome@mail.ru>
2019-11-08 17:39:26 +0300

    test: example test

--- theycome_json/tests/ParserTest.java
+++ theycome_json/tests/ParserTest.java
@@ -952,5 +952,44 @@ public class ParserTest {
     List<String> res3 = parser.getRoot().getStringArray("name\\alsoKnownAs");
     Assert.assertEquals(0, res3.size());
   }
+
+  @Test
+  public void exampleCode() {
+
+    String json = "{" +
+      "\"pageInfo\": {" +
+      "\"totalResultsInt\": 1," +
+      "\"totalResultsString\": \"abc\"}," +
+      "\"items\": [" +
+      "{\"entry\": {}}," +
+      "{\"entry\": {}}" +
+      "]," +
+      "\"name\": {" +
+      "\"alsoKnownAs\": {\"id1\": 1,\"id2\": 2}," +
+      "\"resultsPerPage\": 5," +
+      "\"doubleArray\": [101.01,20.0]" +
+      "}}";
+
+    parser.parse(json);
+
+    String jsonString = parser.toString(); // get output as non-formatted json string
+
+    // Container - is an object we can use to access lower levels of json hierarchy
+    Container container2 = parser.getRoot().get("items").get(1).get("entry");
+    Container container3 = parser.getRoot().get("items[1]\\entry"); // or using unified path
+    Assert.assertEquals("entry", container3.name());
+
+    Container container4 = parser.getRoot().get("items");
+    for (int i = 0; i < container4.entriesSize(); i++) { // iterating collection
+      Container container5 = container4.get(i);
+    }
+
+    // get typed values
+    int val1 = parser.getRoot().getInt("pageInfo\\totalResultsInt");
+    String val2 = parser.getRoot().getString("pageInfo\\totalResultsString");
+
+    // get typed values as arrays
+    List<Double> val3 = parser.getRoot().getDoubleArray("name\\doubleArray");
+  }
 }
 
